e2963ceef155cc183e3d63f4bd35d99a09043b08
Harshi87642 <harshiharshi87642@gmail.com>
2026-01-18 22:11:25 +0530

    first practice commit

---
+++ hr01.java
@@ -0,0 +1,13 @@
+class hr01
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+   }
+}
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~``
+
+OUTPUT:
+
+Hello World
