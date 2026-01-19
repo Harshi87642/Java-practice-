67a07f291473b510cde7e05c158afa425b97f309
Harshi87642 <harshiharshi87642@gmail.com>
2026-01-19 06:48:24 +0530

    simple practice program 2

---
+++ hr02.java
@@ -0,0 +1,31 @@
+// program to demonstrate the working arithmetic operators
+
+public class hr02
+{
+  public static void main(String[] args)
+  {
+   int a=10, b=3, sum, diff, mul, quot, rem;
+    sum= a + b;
+     diff=a - b;
+    mul=a * b;
+     quot=a / b;
+    rem=a % b;
+
+    System.out.println("the sum is : " +sum);
+     System.out.println("the difference  is : " +diff);
+     System.out.println("the multiplication  is : " +mul);
+     System.out.println("the quotient is : " +quot);
+     System.out.println("the remainder is : " +rem);
+   }
+}
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+the sum is : 13
+the difference  is : 7
+the multiplication  is : 30
+the quotient is : 3
+the remainder is : 1
