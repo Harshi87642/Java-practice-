20414a3a24f13592ca67b72e07f1d299d30daea5
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-21 07:29:42 +0530

    simple practice program 6

---
+++ hr06.java
@@ -0,0 +1,22 @@
+// program to study  increment and decrement operators
+
+
+public class hr06
+{
+ public static void main(String[] args)
+ {
+  int a=0, b=10,c,d;
+   c=a + b; d=a-b;
+
+   System.out.println("\n The new value of a is c is: " +c + ++c + c++);
+    System.out.println(c);
+
+  }
+}
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+ The new value of a is c is: 101111
+12
