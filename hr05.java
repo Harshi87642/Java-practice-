6c398a922bce5c5b4969228c5d249b23e457ddac
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-21 07:28:53 +0530

    simple practice program 5

---
+++ hr05.java
@@ -0,0 +1,21 @@
+// program to demonstrate increment and decrement operators
+
+public class hr05
+{
+ public static void main(String[] args)
+  {
+    int a=6;
+     
+   System.out.println("\n The new value of a is " +a + ++a + a++);
+   System.out.println(a);
+
+   }
+}
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+The new value of a is 677
+8
