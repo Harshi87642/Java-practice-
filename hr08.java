39ec5df5d859927ddb392ae40b09eee8c8b4bd3a
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-21 07:30:43 +0530

    simple practice program 8

---
+++ hr08.java
@@ -0,0 +1,25 @@
+// program to demonstrate if else statement
+//program to check whether the given number is even or odd
+
+public class hr08
+{
+  public static void main(String[] args)
+  {
+   int a= Integer.parseInt(args[0]);
+
+   System.out.println("a= " +a);
+
+   if(a % 2 ==0)
+   System.out.println(a+ " is Even");
+   else
+   System.out.println(a+ "is Odd");
+}
+}
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+a= 6
+6 is Even
+
+a= 9
+9is Odd
