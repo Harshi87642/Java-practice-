3c302fb098c5716da3a9319b495bdf3a3023fc98
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-22 12:14:58 +0530

    simple practice program 9

---
+++ hr09.java
@@ -0,0 +1,25 @@
+//program to demonstrate if else statement
+
+public class hr09
+{
+ public static void main(String[] args)
+  {
+   int a= Integer.parseInt(args[0]);
+
+   System.out.println("a = " +a);
+  
+   if(a>=0)
+   System.out.println(a+ "is +ve");
+   else
+   System.out.println(a+ "is -ve");
+ }
+}
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+OUTPUT:
+ 
+a = 9
+9is +ve
+
+a = -33
+-33is -ve
