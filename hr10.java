6896c2fc684ca2b121ed5b9954b13dc2642bae2f
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-22 12:16:14 +0530

    simple practice program 10

---
+++ hr10.java
@@ -0,0 +1,27 @@
+//program to demonstrate if else statement
+
+public class hr10
+{
+ public static void main(String[] args)
+  {
+   int a=-25, b=30;
+
+   System.out.println("a = " +a);
+   System.out.println("b = " +b);
+
+  
+   if(a>=0)
+   System.out.println(a+ "is +ve");
+   else
+   System.out.println(a+ "is -ve");
+ }
+}
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+a = -25
+b = 30
+-25is -ve
+
