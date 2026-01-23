aea8468b773417acc1dc2ea9da500461de95a71d
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-23 10:59:38 +0530

    simple practice program 14

---
+++ hr14.java
@@ -0,0 +1,18 @@
+//program to demonstrare the working of relational operations
+//program to print the larger number
+//program demonstrating the if else statement
+
+public class hr03
+{
+public static void main(String[] args)
+{
+int a = Integer.parseInt(args[0]);
+int b = Integer.parseInt(args[1]);
+
+if(a > b)
+System.out.println(a+ " is big");
+else
+System.out.println(b+ " is big");
+
+}
+}
