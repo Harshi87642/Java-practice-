3333c41f4bbb0fa0b8dc95541b18978dc438b67b
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-23 11:00:23 +0530

    simple practice program 15

---
+++ hr15.java
@@ -0,0 +1,21 @@
+class Student
+{
+  Student()
+   {
+    System.out.println("Default constructor ");
+   }
+}
+public class hr15
+{
+  public static void main(String[] args)
+  {
+     Student s1= new Student();
+   }
+}
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+Default constructor
