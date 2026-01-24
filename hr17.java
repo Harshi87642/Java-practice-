853287984b97a007203be3840747f37d0674d358
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-24 22:44:04 +0530

    simple practice program 17

---
+++ hr17.java
@@ -0,0 +1,24 @@
+// Find the largest of three numbers
+
+class hr16
+{
+  public static void main(String[] args)
+  {
+   int a= 15, b= 25, c= 10;
+   
+   if(a>b && a>c)
+       System.out.println(" A is the largest");
+   else if(b>a && b>c)
+       System.out.println("B is largest");
+    else 
+       System.out.println("C is largest");
+    }
+}
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+B is largest
