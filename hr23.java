d9e6946dd1b1bb0afb10dd95acf0cd7b5af6f444
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-25 20:52:05 +0530

    simple practice program 23

---
+++ hr23.java
@@ -0,0 +1,26 @@
+// Find first sum of N natural numbers
+
+class hr23
+{
+  public static void main(String[] args)
+  {
+    int n = 10;
+    int sum = 0;
+   
+   for(int i=1; i<=n; i++)
+   {
+     sum = sum + i;
+    }
+
+  System.out.println("Sum = " +sum);
+ }
+}
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+
+OUTPUT:
+
+Sum = 55
