5d2700aac270ff8b64266806466f7a1634889772
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-25 20:50:10 +0530

    simple practice program 20

---
+++ hr20.java
@@ -0,0 +1,25 @@
+class hr20
+{
+  public static void main(String[] args)
+  {
+   int num = 123;
+   int sum = 0;
+   
+   while(num != 0)
+   {
+     sum= sum + (num % 10);
+     num = num/10;
+   }
+
+  System.out.println("Sum of digits ="  +sum);
+ }
+}
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+OUTPUT:
+
+Sum of digits = 6
