f6977f5024fdadf59750443544c9c45a0dff94cc
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-26 21:56:32 +0530

    simple practice program 24

---
+++ hr24.java
@@ -0,0 +1,29 @@
+//swap two numbers
+
+class hr24
+{
+  public static void main(String[] args)
+  {
+    int a=5, b=10;
+  
+   a=a+b;
+   b=a-b;
+   a=a-b;
+  
+   Sytsem.out.println("a = " +a);
+   Sytsem.out.println("b = " +b);
+ }
+}
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+OUTPUT:
+
+a = 10
+b = 5
