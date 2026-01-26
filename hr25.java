b931790f44ac5138ca19201656f230d841e088f6
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-26 21:57:04 +0530

    simple practice program 25

---
+++ hr25.java
@@ -0,0 +1,27 @@
+//Check leap year
+
+class hr25
+{
+  public static void main(String[] args)
+  {
+    int year = 2024;
+   
+   if(year % 4 ==0)
+      System.out.println("Leap year");
+
+   else
+     System.out.println("Not a Leap year");
+   }
+}
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+OUTPUT:
+
+Leap year
