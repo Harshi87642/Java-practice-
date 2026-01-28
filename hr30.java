e8ab2b2c1bd35273410529f0ae5cd7105079cea3
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-28 23:28:03 +0530

    simple practice program 30

---
+++ hr30.java
@@ -0,0 +1,21 @@
+//CONVERT CELSIUS TO FARENHEIT
+
+class hr30
+{
+  public static void main(String[] args)
+   {
+     float c =25;
+     float f = (c*9/5)+32;
+
+    System.out.println("Farenheit = " +f)
+   }
+}
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+Farenheit = 77.0
