9f86d0b8bb5be4ab6eeb60107931f677a456e5fc
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-29 22:42:04 +0530

    Simple practice program 33

---
+++ hr33.java
@@ -0,0 +1,21 @@
+// Finding cube of a number
+
+class hr33
+{
+  public static void main(String[] args)
+  {
+    
+     int n = 3;
+     System.out.println("cube = " +(n*n*n));
+   }
+}
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+cube = 27
+ 
