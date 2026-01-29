edce13c80502bbfd4688739723d4fb0ec75640ce
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-29 22:42:29 +0530

    Simple practice program 34

---
+++ hr34.java
@@ -0,0 +1,29 @@
+// Print numbers from 1 to 5
+
+class hr34
+{
+  public static void main(String[] args)
+  {
+     
+    for(int i = 1; i<=5; i++)
+    {
+       System.out.println(i);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+1
+2
+3
+4
+5
