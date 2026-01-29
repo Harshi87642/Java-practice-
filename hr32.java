dc1dc38154423be2e3b14e57a68046980855cadb
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-29 22:41:37 +0530

    Simple practice program 32

---
+++ hr32.java
@@ -0,0 +1,21 @@
+// Find sqaure of a number
+
+class hr32
+{
+  public static void main(String[] args)
+  {
+    
+   int n= 6;
+    System.out.println("Square = "+(n*n));
+  }
+}
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+ 
+Square = 36
