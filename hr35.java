9c9338f5864607b443b6628435ca6024fa4b67ee
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-29 22:42:52 +0530

    Simple practice program 35

---
+++ hr35.java
@@ -0,0 +1,23 @@
+// Find Length of a string
+
+class hr35
+{
+  public static void main(String[] args)
+  {
+    
+    String s = "Java";
+    System.out.println("Length = " +s.length());
+   }
+}
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+OUTPUT:
+
+Length = 4
