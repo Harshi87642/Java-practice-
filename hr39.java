350a37200a3932e46196b7efa7b80124043265a1
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-30 14:40:52 +0530

    Simple practice program 39

---
+++ hr39.java
@@ -0,0 +1,24 @@
+// Simple interest calculation
+
+class hr39
+{
+  public static void main(String[] args)
+  {
+  
+      int p = 1000, r=5,t=2;
+       int si = (p*r*t)/100;
+
+      System.out.println("Simple Interest = " +si);
+   }
+}
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+OUTPUT:
+
+Simple Interest = 100
