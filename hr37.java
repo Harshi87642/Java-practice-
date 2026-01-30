47d81a9b0feb703dae1f5c497f9c59b73b0223fd
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-30 14:39:56 +0530

    Simple practice program 37

---
+++ hr37.java
@@ -0,0 +1,25 @@
+// convert Kilometers to Meters
+
+class hr37
+{
+  public static void main(String[] args)
+  {
+   
+     int km = 3;
+     int meter = km*1000;
+
+    System.out.println("Meters = " +meter);
+   }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+Meters = 3000
