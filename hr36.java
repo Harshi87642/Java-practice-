0458cefc24537258dd863289779c9f0cd46def5c
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-30 14:39:31 +0530

    Simple practice program 36

---
+++ hr36.java
@@ -0,0 +1,25 @@
+// Find maximum of two numbers
+
+class hr36
+{
+  public static void main(String[] args)
+  {
+  
+     int a= 8, b=12;
+    
+      if(a>b)
+         System.out.println("a is maximum");
+      else 
+          System.out.println("b is maximum");
+  
+    }
+}
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+b is maximum
