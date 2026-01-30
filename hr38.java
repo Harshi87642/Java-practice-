a22f938c48858256ef28503e22c5ab413c04e910
Harshitha H R <harshiharshi87642@gmail.com>
2026-01-30 14:40:20 +0530

    Simple practice program 38

---
+++ hr38.java
@@ -0,0 +1,31 @@
+// Print Even Numbers from 1 to 10
+
+
+class hr38
+{
+  public static void main(String[] args)
+   {
+   
+     for( int i = 1; i<= 10; i++)
+      {
+         if(i % 2 == 0)
+              System.out.println(i);
+      }
+  }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+2
+4
+6
+8
+10
