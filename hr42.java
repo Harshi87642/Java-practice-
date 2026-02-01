de74df532c39f121d22cb74042df8c5e71ed5a3a
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-01 22:48:26 +0530

    Simple practice program 42

---
+++ hr42.java
@@ -0,0 +1,22 @@
+// Find Remainder of Two Numbers
+
+
+class hr42
+{
+  public static void main(String[] args)
+  {
+    int a = 17, b = 5;
+  
+     System.out.println("Remainder = " +(a%b));
+  }
+}
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+OUTPUT:
+Remainder = 2
