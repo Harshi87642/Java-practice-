fa56119c3ab66ad9495508c7630f8d498737d3a1
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-01 22:49:24 +0530

    Simple practice program 43

---
+++ hr43.java
@@ -0,0 +1,25 @@
+// Print Character from String
+
+
+class hr43
+{
+  public static void main(String[] args)
+  {
+   
+    String s = "Java";
+
+    System.out.println("First character = " +s.charAt(0));
+   }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+First character = J
