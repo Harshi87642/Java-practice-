d4d7f2f187b03591e74c0108cd612e77688dc60a
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-01 22:47:31 +0530

    Simple practice program 41

---
+++ hr41.java
@@ -0,0 +1,24 @@
+// Convert Minutes to Second
+
+class hr41
+{
+  public static void main(String[] args)
+  {
+  
+      int min = 2;
+      int sec = min*60;
+
+      System.out.println("Seconds = " +sec);
+  }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~``
+
+OUTPUT:
+Seconds = 120
