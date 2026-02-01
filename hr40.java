2890239673c3e172c7b9ef0e785dc3c7d883a213
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-01 22:46:50 +0530

    Simple practice program 40

---
+++ hr40.java
@@ -0,0 +1,25 @@
+// Check Number is Multilple of 5
+
+class hr40
+{
+  public static void main(String[] args)
+   {
+        int n = 25;
+ 
+        if(n % 5 == 0)
+                System.out.println("Multiple of 5");
+         else 
+                 System.out.println("Not a multiple of 5");
+    }
+}
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~``
+
+OUTPUT:
+Multiple of 5
+
