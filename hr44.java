3e7dd14dcbb693f344d083bce9c550969b938726
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-02 22:13:02 +0530

    Simple practice program 44

---
+++ hr44.java
@@ -0,0 +1,27 @@
+// Check Number is divisible by 3
+
+
+class hr44
+{
+  public static void main(String[] args)
+  {
+   
+    int n = 9;
+     
+    if(n%3 == 0)
+       System.out.println("Divisible by 3");
+    else
+       System.out.println("Not divisible by 3");
+   }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+Divisible by 3
