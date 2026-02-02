d2e0c4f164fdcf90e04916a1303d119ea08d3111
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-02 22:13:35 +0530

    Simple practice program 45

---
+++ hr45.java
@@ -0,0 +1,26 @@
+// Convert Days to Hours
+
+class hr45
+{
+  public static void main(String[] args)
+  {
+ 
+     int days = 2;
+     int hours = days*24;
+
+     System.out.println("Hours = "+hours);
+  }
+}
+
+
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+Hours = 48
