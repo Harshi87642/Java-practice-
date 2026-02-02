63c8f2d24bce37bc0c351e619658dd8f98494b2e
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-02 22:14:41 +0530

    Simple practice program 47

---
+++ hr47.java
@@ -0,0 +1,25 @@
+// Check Character is digit or not
+
+class hr47
+{  
+  public static void main(String[] args)
+  { 
+    char ch = '7';
+  
+    if(ch >= '0' && ch <= '9')
+          System.out.println("Digit");
+    else
+         System.out.println("Not a digit");
+  }
+}
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+Digit
