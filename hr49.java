4a4cb05c6d195ab0389113fb89eb5b1373b35dcf
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-03 23:02:37 +0530

    Simple practice program 49

---
+++ hr49.java
@@ -0,0 +1,21 @@
+// Find Area of a Rectangle
+
+class hr49
+{
+  public static void main(String[] args)
+  {
+     int l=5, b=4;
+     int area = l*b;
+
+     System.out.println("Area = "+area);
+   }
+}
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+Area = 20
