2bc8d1710742a18f4a2d3a65c852a3297678deab
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-03 23:02:05 +0530

    Simple practice program 48

---
+++ hr48.java
@@ -0,0 +1,25 @@
+// Reverse a string
+
+class hr48
+{
+ public static void main(String[] args)
+ {
+   String s = "Java";
+
+   for(int i=s.length()-1;i>=0;i--)
+    {
+      System.out.println(s.charAt(i));
+     }
+  }
+}
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+avaJ
