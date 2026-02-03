2f070c1149e19391a60f06db1f8cf1f67f7aafcc
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-03 23:03:47 +0530

    Simple practice program 51

---
+++ hr51.java
@@ -0,0 +1,26 @@
+// Print square numbers from 1 to 5
+
+class hr51
+{
+  public static void main(String[] args)
+   {
+     for(int i=1; i<=5 ; i++)
+     {
+       System.out.println(i*i);
+     }
+   }
+}
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+1
+4
+9
+16
+25
