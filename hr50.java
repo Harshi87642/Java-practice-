f37383fe1ce6a421735d3ab5e43cf4710dcc0f50
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-03 23:03:18 +0530

    Simple practice program 50

---
+++ hr50.java
@@ -0,0 +1,24 @@
+// Check number is greater than 100
+
+
+class hr50
+{
+  public static void main(String[] args)
+  {
+ 
+    int n=120;
+    if(n>100)
+      System.out.println("Greater than 100");
+     else
+       System.out.println("Lesser than or equal to 100");
+   }
+}
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:   Greater than 100
