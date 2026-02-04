064733ec4bdbdbc3462b6c920d5934fdf7d6a6d8
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-04 21:03:21 +0530

    Simple practice program 52

---
+++ hr52.java
@@ -0,0 +1,25 @@
+// Convert centimeters to meters
+
+class hr52
+{ 
+  public static void main(String[] args)
+  {
+   
+     int cm = 250;
+     double meter = cm/100.0;
+
+      System.out.println("Meters = "+meter);
+   }
+}
+
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+Meters = 2.5
