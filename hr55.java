53ce2481aec8eb42a5f8cde60bd9fc59ed50a320
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-04 21:05:50 +0530

    Simple practice program 55

---
+++ hr55.java
@@ -0,0 +1,28 @@
+// Find sum of Even Numbers from 1 to 10
+
+class hr55
+{
+ public static void main(String[] args)
+  {
+    int sum=0;
+    
+    for(int i=1; i<=10; i++)
+     {
+       if(i%2 == 0)
+          sum = sum+i;
+     }
+       System.out.println("Sum of even numbers = "+sum);
+  }
+}
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+Sum of even numbers = 30
+   
