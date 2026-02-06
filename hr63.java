3f4c7b59831909c211ca49737afa69bbb97c9bc1
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-06 09:27:54 +0530

    Simple practice program 63

---
+++ hr63.java
@@ -0,0 +1,28 @@
+// Check Character is alphabet or not
+
+class hr63
+{ 
+   public static void main(String[] args)
+   {
+  
+       char ch = y;
+  
+       if(( ch >= 'a' && ch <='z') || ( ch >= 'A' && ch <='Z'))
+       {
+         System.out.println("It is an Alphabet");
+       }
+       else 
+       {
+        System.out.println(" Not an Alphabet");
+       }
+    }
+}
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+It is an Alphabet
