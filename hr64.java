a46fe3a0851c45f1cf9389c2e2ed56b07d263be5
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-06 09:28:38 +0530

    Simple practice program 64

---
+++ hr64.java
@@ -0,0 +1,24 @@
+// Check Number is 3-digit or not
+
+
+class hr64
+{
+  public static void main(String[] args)
+  {
+    
+      int n=333;
+  
+      if(n>= 100 && n<=999)
+         System.out.println(" It is a 3 digit number");
+      else
+          System.out.println(" It is not a 3 digit number");
+  }
+}
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+It is a 3 digit number
+
