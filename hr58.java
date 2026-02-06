252f2dc19f0735971f4040af1486ca4b08bb3f78
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-06 09:22:54 +0530

    Simple practice program 58

---
+++ hr58.java
@@ -0,0 +1,28 @@
+// Check Eligible for Voting
+
+
+class hr58
+{
+  public static void main(String[] args)
+  {
+     
+      int age = 20;
+   
+      if(age>=18)
+          System.out.println("Eligible for voting");
+      else
+          System.out.println("not Eligible for voting");
+   }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+Eligible for voting
