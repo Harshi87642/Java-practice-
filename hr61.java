9d4691e052e3fec1b5b315becf24dba6466099d0
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-06 09:26:37 +0530

    Simple practice program 61

---
+++ hr61.java
@@ -0,0 +1,26 @@
+// Checkkkk Divisible by 2 and 3
+
+
+class hr61
+{
+   public static void main(String[] args)
+   {
+    
+        int n = 18;
+      
+       if(n%2==0 && n%3==0)
+           System.out.println("divisible by both 2 and 3");
+       else
+           System.out.println(" Not divisible by both 2 and 3");
+      
+    }
+}
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+divisible by both 2 and 3
