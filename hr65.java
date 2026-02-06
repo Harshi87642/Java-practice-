1c50d949fe253665206356ae5972987f2a2f4dc6
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-06 09:29:11 +0530

    Simple practice program 65

---
+++ hr65.java
@@ -0,0 +1,25 @@
+// Check smaller between two numbers
+
+
+class hr65
+{
+  public static void main(String[] args)
+  {
+   
+     int a=30, b=45;
+
+      if(a<b)
+        System.out.println("a is smaller");
+      else 
+        System.out.println("b is smaller");
+   }
+}
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+a is smaller
