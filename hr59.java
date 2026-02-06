34fe6d13bc63d9ba4c8536b96cd4213b43a7fc3d
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-06 09:23:59 +0530

    Simple practice program 59

---
+++ hr59.java
@@ -0,0 +1,36 @@
+
+// Check Positive, Negative or Zero
+
+
+class hr59
+{
+   public static void main(String[] args)
+    {
+       int n=33;
+
+        if(n == 0)
+        {
+          System.out.println("Zero");
+        }
+        else if(n > 0)
+        {
+           System.out.println("positive");
+         }
+        else
+         {
+             System.out.println("Negative");
+         }
+     }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+ 
+positive
