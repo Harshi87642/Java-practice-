e9cab138fc714be945fa2f0640d40c16af9b4183
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-06 09:25:02 +0530

    Simple practice program 60

---
+++ hr60.java
@@ -0,0 +1,28 @@
+//  Find greatest of three numbers
+
+
+class hr60
+{
+  public static void main(String[] args)
+  {
+    
+     int a=30, b=20, c=70;
+    
+     if(a>b && a>c)
+        System.out.println("a is greater");
+     else if(b>a && b>c)
+         System.out.println("b is greater");
+     else
+         System.out.println("c is greater");
+     }
+}
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+c is greater
