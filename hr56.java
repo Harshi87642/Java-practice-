5dc2840d0067f0d25b86a8641832cf817a56f2fb
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-06 09:18:18 +0530

    Simple practice program 56

---
+++ hr56.java
@@ -0,0 +1,33 @@
+// Check Pass or Fail
+
+
+class hr56
+{
+  public static void main(String[] args)
+  {
+   
+      int marks = 40;
+
+       if(marks >=35)
+       {
+         System.out.println("Pass");
+       }
+       else
+       {
+         System.out.println("Fail");
+       }
+   }
+}
+
+
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+Pass
