5980fffd720fb319950fca66291eb77f006b1bb3
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-06 09:21:49 +0530

    Simple practice program 57

---
+++ hr57.java
@@ -0,0 +1,24 @@
+// Find Absolute Value
+
+
+class hr57
+{
+  public static void main(String[] args)
+   {
+       int n = -8;
+       
+       if(n<0)
+          n=-n;
+       
+      System.out.println("Absolute value = "+n);
+   }
+}
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+Absolute value = 8
