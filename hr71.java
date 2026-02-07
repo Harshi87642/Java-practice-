5338102ba9de95cc32782db2e3ee8a45d6c89518
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-07 23:47:04 +0530

    Simple practice program 71

---
+++ hr71.java
@@ -0,0 +1,27 @@
+// Sum of Numbers 1 to 5
+
+
+class hr71
+{
+  public static void main(String[] args)
+  {
+  
+     for(int i=1; i<=5; i++)
+     {
+       sum=sum+i;
+     }
+    System.out.println("Sum = " +sum);
+ }
+}
+
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+Sum = 15
