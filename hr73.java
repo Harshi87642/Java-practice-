abeeca41e00e9dcf7cc8ce6356265c6f54468f21
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-07 23:48:22 +0530

    Simple practice program 73

---
+++ hr73.java
@@ -0,0 +1,29 @@
+// print Hello 5 times
+
+class hr73
+{
+ public static void main(String[] args)
+ {
+   
+     for(int i=1; i<=5; i++)
+     {
+       System.out.println("Hello");
+     }
+  }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+Hello
+Hello
+Hello
+Hello
+Hello
