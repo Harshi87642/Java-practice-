0af31b49fb415d276e13292d0e01e0808ee890fd
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-07 23:45:37 +0530

    Simple practice program 69

---
+++ hr69.java
@@ -0,0 +1,40 @@
+// Print Odd Numbers(1 to 20)
+
+class hr69
+{
+  public static void main(String[] args)
+  {
+
+    for(int i=1; i<=20; i++)
+    {
+       if(i%2!=0)
+       {
+          System.out.println(i);
+       }
+     }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
