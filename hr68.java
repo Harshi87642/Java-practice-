133678c95e47f0325206c828f2ad56f74e812b4b
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-07 23:44:39 +0530

    Simple practice program 68

---
+++ hr68.java
@@ -0,0 +1,36 @@
+// Print Even Numbers(1 to 20)
+
+class hr68
+{
+  public static void main(String[] args)
+  {
+
+    for(int i=1; i<=20; i++)
+    {
+       if(i%2==0)
+       {
+          System.out.println(i);
+       }
+     }
+  }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
