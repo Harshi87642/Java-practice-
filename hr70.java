b1ba2d5fcd011bef89efece76ca708fd8405f3c8
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-07 23:46:09 +0530

    Simple practice program 70

---
+++ hr70.java
@@ -0,0 +1,40 @@
+// Print Table of 2
+
+class hr70
+{
+  public static void main(String[] args)
+  {
+
+    for(int i=1; i<=10; i++)
+    {
+       System.out.println("2x"+i+"=" +(2*i));
+       
+     }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+2x1=2
+2x2=4
+2x3=6
+2x4=8
+2x5=10
+2x6=12
+2x7=14
+2x8=16
+2x9=18
+2x10=20
