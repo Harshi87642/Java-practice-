65fecb88071e3213648f5422af0f3a80e5a42e43
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-07 23:47:54 +0530

    Simple practice program 72

---
+++ hr72.java
@@ -0,0 +1,32 @@
+// Print square numbers
+
+
+class hr72
+{
+  public static void main(String[] args)
+  {
+  
+    for(int i=1; i<=5; i++)
+     {
+       sqaure = i*i;
+      }
+   System.out.println(square);
+ }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+OUTPUT:
+1
+4
+9
+16
+25
+
