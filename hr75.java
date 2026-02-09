058cab5dadcdd0739e6e10597f78e2c9adf45acc
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-09 23:57:25 +0530

    Simple practice program 75

---
+++ hr75.java
@@ -0,0 +1,25 @@
+// Ternary Operator
+
+class hr75
+{
+  public static void main(String[] args)
+  {
+    
+     int n=4;
+     int result=0;
+
+     result=n%2==0 ? 10 :20;
+      System.out.println(result);
+   }
+}
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+10
