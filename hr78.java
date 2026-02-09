8ce0aaa609c372eb3dfa1e22c0587bafa833f319
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-09 23:58:50 +0530

    Simple practice program 78

---
+++ hr78.java
@@ -0,0 +1,50 @@
+// Print only Wednesday
+
+
+class hr78
+{
+  public static void main(String[] args)
+  {
+
+     int n=3;
+
+     switch(n){
+          case 1:
+              System.out.println("Monday");
+              Break;
+          case 2:
+              System.out.println("Tuesday");
+               Break;
+          case 3:
+              System.out.println("Wednesday");
+               Break;
+           case 4:
+              System.out.println("Thursday");
+              Break;
+           case 5:
+              System.out.println("Friday");
+              Break;
+           case 6:
+              System.out.println("Saturday");
+              Break;
+          case 7:
+              System.out.println("Sunday");
+              Break;
+          default:
+               System.out.println("Invalid")
+              
+          }
+    }
+}
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTPUT:
+
+Wednesday
