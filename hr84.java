0608ec3571f971afe470ebc11ec19019f153d48c
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-10 23:56:31 +0530

    Simple practice program 84

---
+++ hr84.java
@@ -0,0 +1,27 @@
+// print square pattern
+
+class hr84
+{
+  public static void main(String[] args)
+  {
+ 
+      for(int i=1; i<=3; i++)
+      {
+         for(int j=1; j<=3; j++)
+          {
+             System.out.println("* ");
+          }
+       System.out.println();
+      }
+  }
+}
+
+
+
+
+```````````````````````````````````````````
+
+OUTPUT:
+* * *
+* * *
+* * *
