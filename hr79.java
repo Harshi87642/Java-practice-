dc8b8e99dcaf5acac94cdaadfc91b2253bf061df
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-10 23:53:14 +0530

    Simple practice program 79

---
+++ hr79.java
@@ -0,0 +1,28 @@
+// For loop
+
+class hr79
+{
+  public static void main(String[] args)
+  {
+   
+      for(int i=1; i<=5; i++)
+      {
+         System.out.println(i);
+       }
+    }
+}
+
+
+
+
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+OUTPUT:
+1
+2
+3
+4
+5
