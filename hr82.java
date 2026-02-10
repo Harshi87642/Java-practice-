89734fc6a4b6e104b3a81dfbfa84335d2d39cf64
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-10 23:55:20 +0530

    Simple practice program 82

---
+++ hr82.java
@@ -0,0 +1,31 @@
+//Print Even numbers using while loop
+
+
+class hr82
+{
+  public static void main(String[] args)
+   {
+ 
+      int i=1;
+ 
+       while(i%2==0 && i<=10)
+       {
+         System.out.println(i);
+          i++;
+        }
+    }
+}
+
+
+
+
+
+
+`````````````````````````````````````````````````````````````
+
+OUTPUT:
+2
+4
+6
+8
+10
