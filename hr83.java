b3fb9016dbad1bdcff764debf93923e98779a670
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-10 23:55:51 +0530

    Simple practice program 83

---
+++ hr83.java
@@ -0,0 +1,40 @@
+//print Hello 3 times using do while
+
+
+class hr83
+{
+  public static void main(String[] args)
+  {
+
+      int i=1;
+
+      do{
+         System.out.println("Hi "+i);
+          i++;
+        }
+        while(i<=3);
+      }
+}
+
+
+
+
+
+
+
+```````````````````````````````````````````````````````````````````````````````````
+
+OUTPUT:
+
+Hi 1
+Hi 2
+Hi 3
+
+
+
+
+
+
+
+
+      
