d52a4d50c2cbff7c0d2decb4251c1ddd96968279
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-10 23:56:54 +0530

    Simple practice program 85

---
+++ hr85.java
@@ -0,0 +1,28 @@
+//Print number pattern
+
+class hr85
+{
+  public static void main(String[] args)
+  {
+      for(int i=1; i<=3;i++)
+       {
+          for(int j=1; j<=3;j++)
+           {
+              System.out.println(j + " ");
+            }
+            System.out.println();
+        }
+   }
+}
+
+
+
+
+
+
+
+```````````````````````````````````````````````````````````````````
+OUTPUT:
+1 2 3
+1 2 3
+1 2 3
