97e04d92b0b9e5284c5a6ccf67f1a03e7a05b1f3
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-10 23:54:43 +0530

    Simple practice program 81

---
+++ hr81.java
@@ -0,0 +1,30 @@
+// printing 1 to 5 using while loop
+
+class hr81
+{
+  public static void main(String[] args)
+  {
+      int i=1;
+
+     while(i<=5)
+     {
+        System.out.println(i);
+         i++;
+     }
+  }
+}
+
+
+
+
+
+
+
+````````````````````````````````````````````````````````````````````````````
+
+OUTPUT:
+1
+2
+3
+4
+5
