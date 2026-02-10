6aba2cbb27ea3a1fe3d41b69c6904c23a42136bf
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-10 23:54:18 +0530

    Simple practice program 80

---
+++ hr80.java
@@ -0,0 +1,28 @@
+// sum of numbers 1 to 5
+
+class hr80
+{
+  public static void main(String[] args)
+  {
+ 
+     for(int i=1; i<=5; i++)
+     {
+          sum=sum+i;
+      }
+
+    System.out.println(sum);
+  }
+}
+
+
+
+
+
+
+
+
+
+````````````````````````````````````````````````````````````````````````````
+OUTPUT:
+
+15
