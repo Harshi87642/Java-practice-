e2acbbb748c992cc8261bc837b240fe633433721
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-11 23:55:33 +0530

    Simple practice program 88

---
+++ hr88.java
@@ -0,0 +1,37 @@
+// Adding of two numbers
+
+public class Calculator
+{
+     private int num1=10;
+     private int num2=20;
+
+     public int add()
+     {
+         int result= num1+num2;
+          return result;
+      }
+}
+
+public class hr88
+{
+  public static void main(String[] args)
+  {
+ 
+    Calculator calc = new  Calculator();
+
+     calc.add();
+   }
+}
+
+
+
+
+
+
+
+
+````````````````````````````````````````````````````````````````````
+
+OUTPUT:
+
+30
