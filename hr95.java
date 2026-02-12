fc08ecfe7a668b082ea5455c226f8b51e0ab64c1
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-12 23:51:44 +0530

    Simple practice program 95

---
+++ hr95.java
@@ -0,0 +1,31 @@
+// Methos to check even or odd
+
+public class Evenorodd
+{
+   public void check(int n)
+   {
+     if(n%2==0)
+         System.out.println("Even");
+     else
+          System.out.println("Odd");
+     }
+}
+
+public class hr95
+{
+  public static void main(String[] args)
+  {
+  
+       Evenorodd e = new Evenorodd();
+   
+        String str =e.check(5);
+      
+        System.out.println(str);
+   }
+}
+
+
+    
+   
+       
+  
