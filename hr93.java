58a6e1d44acec6da3821c3e51e9c40833b3c37f7
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-12 23:50:13 +0530

    Simple practice program 93

---
+++ hr93.java
@@ -0,0 +1,21 @@
+// Method to Subtract two numbers
+
+public class Addme
+{
+  
+   public void add()
+   {
+       int a=10, b=20;
+        System.out.println("Subtraction = "+(b-a));
+   }
+}
+
+public class hr93
+{
+  public static void main(String[] args)
+  {
+ 
+     Addme a=new Addme();
+      a.add();
+    }
+}
