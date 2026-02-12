1508c4f2df174e67c6aef2083f0bef44e95ca10b
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-12 23:50:58 +0530

    Simple practice program 94

---
+++ hr94.java
@@ -0,0 +1,22 @@
+// Method to multiply 2 numbers
+
+public class multiply
+{
+   
+   public int multiply(int a, int b)
+   {
+       return a*b;
+   }
+}
+
+public class hr94
+{
+   public static void main(String[] args)
+   {
+     
+       multiply m=new multiply();
+      
+        int result = m.multiply(4,5);
+      System.out.println(result);
+    }
+}
