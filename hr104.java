d82ec9e138996f8780448d1db8f6c6a2f88297af
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-15 23:53:25 +0530

    Simple practice program 104

---
+++ hr104.java
@@ -0,0 +1,21 @@
+// Arrays using for each loop
+
+public class hr104
+{
+  public static void main(String[] args)
+  {
+ 
+    int[] arr={10,20,30};
+
+     for(a:arr)
+      {
+         System.out.println(a);
+      }
+  }
+}
+
+
+OUTPUT:
+10
+20
+30
