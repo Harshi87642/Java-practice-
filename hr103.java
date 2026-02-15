680514ccc981980ffb67cf560d3acde95438e51d
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-15 23:53:00 +0530

    Simple practice program 103

---
+++ hr103.java
@@ -0,0 +1,30 @@
+class class hr102
+{
+  public static void main(String[] args)
+  {
+  
+     int[] arr={12,23,35,26,25};
+
+       for(int i=0; i<=arr.length; i++)
+        {
+            System.out.println(i);
+         }
+       System.out.println("number at 3rd index: "arr[3]);
+    }
+}
+
+
+
+
+
+
+
+
+````````````````````````````````````````````````````
+OUTPUT:
+12
+23
+35
+26
+25
+number at 3rd index: 26
