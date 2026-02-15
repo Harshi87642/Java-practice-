919a0a07fe18e5eb3eca48a17201cc97838efc6b
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-15 23:52:36 +0530

    Simple practice program 102

---
+++ hr102.java
@@ -0,0 +1,32 @@
+// Arrays concept
+
+class class hr102
+{
+  public static void main(String[] args)
+  {
+  
+     int[] arr={12,23,35,26,25};
+
+       for(int i=0; i<=arr.length; i++)
+        {
+            System.out.println(i);
+         }
+       
+    }
+}
+
+
+
+
+
+
+
+
+`````````````````````````````````````````````````````````````
+OUTPUT:
+
+12
+23
+35
+26
+25
