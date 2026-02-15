7cff2c2c5f118032921a83d38901654ba041b5d5
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-15 23:58:51 +0530

    Simple practice program 106

---
+++ hr106.java
@@ -0,0 +1,33 @@
+// Printing array in reverse order
+
+public class hr105
+{
+  public static void main(String[] args)
+  {
+      int[] arr={10,20,30};
+       int[] a=arr;
+ 
+       for(int i=arr.length;i>=0;i--)
+        {
+           System.out.println(i);
+         }
+
+
+      System.out.println("c"arr[0]);
+    }
+}
+
+
+
+
+
+
+
+
+`````````````````````````````````````````````````````
+
+OUTPUT:
+30
+20
+10
+Value at index 0: 10
